0474a301fbd89c370cd74506478e274fe5310352
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-04-30 22:58:43 +0500

    LRU cache class getItem method

--- src/modules/lru.ts
+++ src/modules/lru.ts
@@ -1,6 +1,12 @@
-export class LRU_cache {
+export class LRU_cache<T> {
   private maxCacheSize: number;
+  private cache: Map<string, T>;
   constructor(maxCacheSize: number) {
     this.maxCacheSize = maxCacheSize;
+    this.cache = new Map();
+  }
+
+  public getItem(key: string): T | undefined {
+    return this.cache.get(key);
   }
 }
