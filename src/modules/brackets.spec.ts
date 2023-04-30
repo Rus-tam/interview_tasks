da63d0c0426a4750288ab50f59fa022547ad47a8
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-04-30 20:25:13 +0500

    test for checkPairBrackets

--- src/modules/brackets.spec.ts
+++ src/modules/brackets.spec.ts
@@ -1,11 +1,28 @@
 import { checkBracketsPair } from './brackets.ts';
+import exp from 'constants';
 
 describe('Testing checkBracketsPair function', () => {
   test('Should check the pairing of parentheses and receive `Correct`', () => {
-    expect(checkBracketsPair('fhgjf(gjkd[90tifj]gjfkd)')).toBe('Correct \n');
+    expect(checkBracketsPair('bla-bla(bla[90bla]bla-bla-bla/)')).toBe('Correct \n');
   });
 
   test('Should check the pairing of parentheses and receive `Incorrect`', () => {
-    expect(checkBracketsPair('({ghfjsrit)}')).toBe('Incorrect \n');
+    expect(checkBracketsPair('({bla_bla_bla)}')).toBe('Incorrect \n');
+  });
+
+  test('Should check the pairing of parentheses and receive `Incorrect`', () => {
+    expect(checkBracketsPair('(17272(bla-bla)')).toBe('Incorrect \n');
+  });
+
+  test('Should check the pairing of parentheses and receive `Incorrect`', () => {
+    expect(checkBracketsPair('(123[bla)bla]bla'));
+  });
+
+  test('Should check the pairing of parentheses and receive `Correct`', () => {
+    expect(checkBracketsPair('{bla(bla-bla[123])}')).toBe('Correct \n');
+  });
+
+  test('Should check the pairing of parentheses and receive `Correct`', () => {
+    expect(checkBracketsPair('[{}()]')).toBe('Correct \n');
   });
 });
